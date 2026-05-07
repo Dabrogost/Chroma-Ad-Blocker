72f605ece265c60e870f032d873d24411f44b6bf
Dabrogost <kernskaden@gmail.com>
2026-05-07 16:33:55 -0400

    update 1.0.1

--- extension/ui/components.js
+++ extension/ui/components.js
@@ -329,7 +329,7 @@ const ChromaComponents = (() => {
           <a href="https://github.com/Dabrogost/Chroma-Ad-Blocker" target="_blank" class="github-link" title="View Source on GitHub">
             ${githubIcon}
           </a>
-          <span class="version" id="versionText">v1.0.0 &middot; MV3</span>
+          <span class="version" id="versionText">v1.0.1 &middot; MV3</span>
         </div>
       </footer>
     `;
